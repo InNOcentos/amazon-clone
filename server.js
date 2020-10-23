0e681966305808a6ec2000089252962b22e1c134
sboris <smrnvbrs@gmail.com>
2020-10-24 00:34:14 +0300

    added Product schema

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const config = require('./config');
 
+const User = require('./models/user');
+
 mongoose.connect(`mongodb+srv://${config.db_user}:${config.db_password}@amazon-clone.lllez.mongodb.net/${config.db_name}?retryWrites=true&w=majority`,{useNewUrlParser: true, useUnifiedTopology: true},(err)=> {
     if (err) {
         console.log(err);
@@ -22,8 +24,20 @@ app.get('/',(req,res)=> {
     res.send('Hi')
 })
 
-app.post('/',(req,res)=> {
-    res.send('Hi')
+app.post('/',async (req,res)=> {
+    const {name,email,password} = req.body;
+    let user = new User({
+        name,
+        email,
+        password
+    });
+    await user.save(err=> {
+        if(err){
+            res.json(err)
+        } else {
+            res.json('Sucessfully saved');
+        }
+    }) 
 })
 
 app.listen(3000, (err)=> {
